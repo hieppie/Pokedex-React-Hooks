00172cab163858d29b2a5d894a730cb0aa96c791
hieppie <hiepqduong@gmail.com>
2022-08-11 12:29:54 -0400

    added pokemon cards

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect  } from "react";
+import PokemonList from "./PokemonList";
+import axios from "axios";
+import Pagination from "./Pagination";
+import PokemonThumbNails from "./PokemonThumbNails";
 
 function App() {
+  const [pokemon, setPokemon] = useState([])
+  const [currentPageUrl, setCurrentPageUrl] = useState("https://pokeapi.co/api/v2/pokemon")
+  const [nextPageUrl, setNextPageUrl] = useState()
+  const [prevPageUrl, setPrevPageUrl] = useState()
+  const [loading, setLoading]= useState(true)
+
+  const[allPokemons, setAllPokemons] = useState([])
+  const [loadMore, setLoadMore] = useState('https://pokeapi.co/api/v2/pokemon?limit=10')
+
+ const getAllPokemons = async () => {
+   const res = await fetch(loadMore)
+   const data = await res.json()
+
+   setLoadMore(data.next)
+
+   function createPokemonObject(results)  {
+     results.forEach( async pokemon => {
+       const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon.name}`)
+       const data =  await res.json()
+       setAllPokemons( currentList => [...currentList, data])
+       await allPokemons.sort((a, b) => a.id - b.id)
+     })
+   }
+   createPokemonObject(data.results)
+ }
+
+useEffect(() => {
+ getAllPokemons()
+}, [])
+
+
+  useEffect(() => {
+    getAllPokemons()
+    setLoading(true)
+    let cancel
+    axios.get(currentPageUrl, {
+      cancelToken: new axios.CancelToken(c => cancel = c)
+    }).then(res => {
+      setLoading(false)
+      setNextPageUrl(res.data.next)
+      setPrevPageUrl(res.data.previous)
+      setPokemon(res.data.results.map(p => p.name))
+    })
+
+    return ()=> cancel()
+  }, [currentPageUrl])
+
+  function goToNextPage(){
+    setCurrentPageUrl(nextPageUrl)
+  }
+
+  function goToPrevPage(){
+    setCurrentPageUrl(prevPageUrl)
+  }
+
+  if (loading) return "Loading..."
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+ <div className="app-container">
+      <h1>Pokemon Evolution</h1>
+      <div className="pokemon-container">
+        <div className="all-container">
+          {allPokemons.map( (pokemonStats, index) => 
+            <PokemonThumbNails
+              key={index}
+              id={pokemonStats.id}
+              image={pokemonStats.sprites.other.dream_world.front_default}
+              name={pokemonStats.name}
+              type={pokemonStats.types[0].type.name}
+            />)}
+        </div>
+          {/* <button className="load-more" onClick={() => getAllPokemons()}>Load more</button> */}
+      </div>
+    
+   {/* <PokemonList pokemon={pokemon} /> */}
+   <Pagination 
+   goToNextPage= {nextPageUrl ? goToNextPage : null}
+   goToPrevPage= {prevPageUrl ? goToPrevPage : null}
+   />
+ </div>
   );
 }
 
